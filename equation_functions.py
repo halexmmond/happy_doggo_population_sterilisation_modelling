47c430ac9fb406661e67fa0c37149fd841f015a4
Alexandra Neville <alexandra.ah.hammond@gmail.com>
2024-04-20 08:25:19 +0100

    Updates to equations for population and total sterilised

--- equation_functions.py
+++ equation_functions.py
@@ -5,16 +5,19 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Number of sterilised dogs
 def num_sterilised_dogs(S0, s_a, m, n_s, t):
-    S_t = (n_s + ((m + s_a - 1) * S0)) * t
+    S_t = ((n_s + ((m + s_a - 1) * S0)) * t) + S0
     return S_t
 
 
 # Total population of stray dogs as a differential equation
 def dNdt(N_t, k, p_f, s_a, s_i, l, r_r, S0, n_s, t, m):
-    dN_dt = (N_t * ((k - N_t) / k) * ((p_f * s_a * s_i * l * r_r * (1 - (((n_s + ((m + s_a - 1) * S0)) * t) / N_t))) - (1 - s_a) + m))
+    dN_dt = (N_t * ((k - N_t) / k) * (
+            (p_f * s_a * s_i * l * r_r * (1 - (((n_s + ((m + s_a - 1) * S0)) * t) / N_t))) - (1 - s_a) + m))
     return dN_dt
+
 
 # Runge Kutta method of solving the differential equation
 def runge_kutta(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, n_s):
@@ -27,41 +30,66 @@ def runge_kutta(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, n_s):
     t_values = [t0]
     N_t_values = [N0]
     S_t_values = [S0]
-    S_N_values = [S0/N0]
+    S_N_values = [(S0 / N0) * 100]
 
     # Iterate using fourth order Runge-Kutta
-    while t < desired_t:
+    while t < desired_t+1:
         # Calculate k1, k2, k3, k4
         k1 = h * dNdt(t=t, N_t=N_t, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
-        k2 = h * dNdt(t = t + 0.5 * h, N_t = N_t + 0.5 * k1, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
-        k3 = h * dNdt(t = t + 0.5 * h, N_t = N_t + 0.5 * k2, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
-        k4 = h * dNdt(t = t + h, N_t = N_t + k3, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
+        k2 = h * dNdt(t=t + 0.5 * h, N_t=N_t + 0.5 * k1, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m,
+                      n_s=n_s)
+        k3 = h * dNdt(t=t + 0.5 * h, N_t=N_t + 0.5 * k2, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m,
+                      n_s=n_s)
+        k4 = h * dNdt(t=t + h, N_t=N_t + k3, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
 
         # Update N using weighted average of k's
         N_t = N_t + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+        #if N_t < 0:
+        #    N_t = 0
 
         # Update t
         t = t + h
 
         # Update S(t)
-        S_t = (n_s + ((m + s_a - 1) * S0)) * t
+        S_t = ((n_s + ((m + s_a - 1) * S0)) * t) + S0
+
         if S_t > N_t:
             S_t = N_t
 
+        #if S_t < 0:
+        #    S_t = 0
+
         # Update S(t)/N(t)
+        #if N_t == 0:
+        #    S_N_t = 0
+        #else:
+        #    S_N_t = (S_t / N_t) * 100
 
         # Store values in list
         t_values.append(t)
         N_t_values.append(N_t)
         S_t_values.append(S_t)
-        S_N_values.append(S_t/N_t*100)
+        S_N_values.append((S_t / N_t) * 100)
 
         # Check if t is close enough to desired_t
         if abs(t - desired_t) < 0.01:
             break
 
         # Optionally, you can print or store the values of t and N_t
-        #print(f"t = {t}, N_t = {N_t}")
+        # print(f"t = {t}, N_t = {N_t}")
+
+    return math.ceil(N_t), t_values, N_t_values, S_t_values, S_N_values
+
+
+# This function uses the Runge-Kutta method to plot the population and sterilisation growth on a graph
+def graph_runge_kutta(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, n_s):
+
+    population_model = runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
+    t_values = population_model[1]
+    N_t_values = population_model[2]
+    S_t_values = population_model[3]
+    S_N_values = population_model[4]
 
     # Plotting
     plt.figure(figsize=(10, 6))
@@ -88,6 +116,70 @@ def runge_kutta(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, n_s):
     plt.legend()
     plt.show()
 
-    return N_t
+
+# This equation calculates the number of sterilisations required per month to reach a target sterilisation proportion
+# within a specified timeframe using binary search
+def growth_binary_search_n_s(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, desired_S_N):
+
+    lower_limit = 0
+    upper_limit = 2000
+
+    while upper_limit - lower_limit > 1:
+        # Try n_s at the midrange point
+        n_s = math.ceil((upper_limit + lower_limit) / 2)
+
+        # Calculate S(t) and N(t) at desired time using chosen n_s
+        S_t = ((n_s + ((m + s_a - 1) * S0)) * desired_t) + S0
+
+        N_t = runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
+        if N_t < 0:
+            N_t = 1
+
+       # Calculate sterilisation proportion
+        S_N = S_t / N_t
+
+        if S_N < desired_S_N:
+            lower_limit = n_s
+        else:
+            upper_limit = n_s
+
+    return n_s + 1
 
 
+# This function calculates the derivative of S(t)/N(t)
+def dSNdt(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m, n_s):
+    S_t = ((n_s + ((m + s_a - 1) * S0)) * desired_t) + S0
+
+    dS_dt = n_s + ((m + s_a - 1) * S0)
+
+    N_t = runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m,
+                      n_s=n_s)
+
+    dN_dt = (N_t * ((k - N_t) / k) * (
+                (p_f * s_a * s_i * l * r_r * (1 - (((n_s + ((m + s_a - 1) * S0)) * desired_t) / N_t))) - (1 - s_a) + m))
+
+    dSN_dt = ((N_t * dS_dt) - (S_t * dN_dt)) / N_t ** 2
+
+    return dSN_dt
+
+
+# This function finds a value for n_s which keeps the sterilisation proportion at equilibrium
+def maintenance_binary_search_n_s(t0, N0, desired_t, h, k, p_f, s_a, s_i, l, r_r, S0, m):
+    lower_limit = 0
+    upper_limit = 2000
+
+    while upper_limit - lower_limit > 1:
+        # Try n_s at the midrange point
+        n_s = math.ceil((upper_limit + lower_limit) / 1.5)
+
+        # Calculate the derivative of S(t)/N(t) across the desired timeframe
+        dSN_dt = dSNdt(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m,
+                       n_s=n_s)
+        print(dSN_dt)
+
+        if dSN_dt < 0:
+            lower_limit = n_s
+        else:
+            upper_limit = n_s
+
+    return n_s + 1
--- run_model.py
+++ run_model.py
@@ -1,23 +1,34 @@
 from equation_functions import dNdt
 from equation_functions import runge_kutta
+from equation_functions import growth_binary_search_n_s
+from equation_functions import maintenance_binary_search_n_s
+from equation_functions import graph_runge_kutta
+import equation_functions
 
 # Choose values for equations to test plotting and estimation
-t0 = 0            # Initial time
-N0 = 2000         # Initial total population
-desired_t = 30    # Time over which to model (in months)
-h = 1             # Step size for Runge Kutta method (1 recommended)
-k = 10000         # Carrying capacity of environment
-p_f = 0.5         # Proportion of population that are female (0-1)
-s_a = 0.99        # Adult survivability rate (0-1)
-s_i = 0.4         # Infant survivability rate (0-1)
-l = 6             # Reproduction rate/average litter size
-r_r = 2/12        # Average number of litters (monthly)
-S0 = 0            # Initial number of sterilised dogs
-m = 0             # Net migration rate (positive into environment, negative out) (0-1)
-n_s = 220         # Number of sterilisations taking place each month
-# N_t             # Population at time t
-# S_t             # Number of sterilised dogs at time t
+t0 = 0                 # Initial time
+N0 = 1000              # Initial total population
+desired_t = 24         # Time over which to model (in months)
+h = 1                  # Step size for Runge Kutta method (1 recommended)
+k = 10000              # Carrying capacity of environment
+p_f = 0.5              # Proportion of population that are female (0-1)
+s_a = 0.99             # Adult survivability rate (0-1)
+s_i = 0.4              # Infant survivability rate (0-1)
+l = 6                  # Reproduction rate/average litter size
+r_r = 2/12             # Average number of litters (monthly)
+S0 = 800               # Initial number of sterilised dogs
+m = 0                  # Net migration rate (positive into environment, negative out) (0-1)
+n_s = 100              # Number of sterilisations taking place each month
+desired_S_N = 0.8      # Desired sterilisation proportion
+# N_t                  # Population at time t
+# S_t                  # Number of sterilised dogs at time t
 
 # add more here for sterilisation proportion when new code is added
 
-runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
+#print(runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s))
+
+graph_runge_kutta(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, n_s=n_s)
+
+#print(growth_binary_search_n_s(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m, desired_S_N=desired_S_N))
+
+#print(maintenance_binary_search_n_s(t0=t0, N0=N0, desired_t=desired_t, h=h, k=k, p_f=p_f, s_a=s_a, s_i=s_i, l=l, r_r=r_r, S0=S0, m=m))
