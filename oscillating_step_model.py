528a70c5bb66222a5703274fff512a7805654738
Alexandra Neville <alexandra.ah.hammond@gmail.com>
2024-07-31 10:26:53 +0100

    Adding equations and starting to build

--- oscillating_step_model.py
+++ oscillating_step_model.py
@@ -25,7 +25,7 @@ p_f = 0.5
 s_i = 0.4
 lifespan = 72 # lifespan in months
 p_lifespan = 0.24 # proportion of dogs that reach lifespan
-initial_adult_prop = 0
+initial_p_adult = 0
 l = 6
 r_r = 2
 m_net_in = 0
@@ -35,21 +35,34 @@ t0 = 0
 N_list = [N0]
 S_list = [N0 * initial_ster_prop]
 ster_prop_list = [initial_ster_prop] # Note we are currently saying equally likely for adult and puppy to get sterilised
-N_adult_list = [N0 * initial_adult_prop]
-N_puppy_list = [N0 * (1 - initial_adult_prop)]
+N_adult_list = [N0 * initial_p_adult]
+N_puppy_list = [N0 * (1 - initial_p_adult)]
 monthly_births_list = [0]
 t_list = [t0]
 p_adult_list = [initial_p_adult]
 
-# Adult monthly survivability
-# This formula calculates s_a_month
-s_a_month = 1 - ((1 - p_lifespan) / (lifespan - 12))
+### Functions
 
-# Births per month
+# Adult monthly survivability
+def adult_monthly_survivability(p_lifespan, lifespan):
+    s_a_month = 1 - ((1 - p_lifespan) / (lifespan - 12))
+    return s_a_month
+
+# Monthly births
 # i.e. total pop two months ago x logistic factor x female prop x monthly adult survivability
 # x infant survivability x average litter size x litters per month x adult prop 2 months ago
 # x sterilisation proportion 2 months ago (assuming same prop sterilised under and over 1)
-monthly_births = N_list[t-2] * ((k - N_list[t-2]) / k) * p_f * s_a_month * s_i * l * (r_r/12) * p_adult_list[t-2] * (1 - (S_list[t-2]/N_list[t-2]))
+def monthly_births(k, p_f, s_a_month, s_i, l, r_r, N_total, p_adult, S_total):
+    births = N_total * ((k - N_total) / k) * p_f * s_a_month * s_i * l * (r_r/12) * p_adult * (1 - (S_total/N_total))
+    # Note that this is the formula we are calculating
+    # monthly_births = N_list[t-2] * ((k - N_list[t-2]) / k) * p_f * s_a_month * s_i * l * (r_r/12) * p_adult_list[t-2] * (1 - (S_list[t-2]/N_list[t-2]))
+    # We need to make sure we have the correct index for t
+    return births
+
+#
+
+
+
 
 # Deaths per month
 # i.e. total pop one month ago x adult prop one month ago x monthly adult mortality +
