b4a0f550427c079213a0e405c7f3b534092baf58
Alexandra Neville <alexandra.ah.hammond@gmail.com>
2024-07-31 10:41:49 +0100

    Adding equations and starting to build

--- oscillating_step_model.py
+++ oscillating_step_model.py
@@ -4,42 +4,6 @@
 
 # This file will follow the same steps as oscillating_model.py but with the step model instead
 
-### Functions
-
-# Calculate number of sterilised dogs for initial calculation from initial sterilisation rate
-# Number of sterilised dogs
-def num_sterilised_dogs(initial_S_N, N0, s_a, m, n_s, t):
-    S0 = N0 * initial_S_N
-    S_t = ((n_s + ((m + s_a - 1) * S0)) * t) + S0
-    return S_t
-
-
-### Code
-
-# We need to create lists I'm thinking to store the data in
-# Try this way instead of dataframes and see what happens - should still be able to replace at certain index
-
-N0 = 1000
-initial_ster_prop = 0.8
-p_f = 0.5
-s_i = 0.4
-lifespan = 72 # lifespan in months
-p_lifespan = 0.24 # proportion of dogs that reach lifespan
-initial_p_adult = 0
-l = 6
-r_r = 2
-m_net_in = 0
-k = 100000
-t0 = 0
-
-N_list = [N0]
-S_list = [N0 * initial_ster_prop]
-ster_prop_list = [initial_ster_prop] # Note we are currently saying equally likely for adult and puppy to get sterilised
-N_adult_list = [N0 * initial_p_adult]
-N_puppy_list = [N0 * (1 - initial_p_adult)]
-monthly_births_list = [0]
-t_list = [t0]
-p_adult_list = [initial_p_adult]
 
 ### Functions
 
@@ -103,3 +67,31 @@ def adult_population(N_adult_one_month_ago, births_t_one_year_ago, deaths_t, net
     # N_adult = N_adult_list[t-1] + monthly_births_list[t-12] - monthly_deaths[t] + (net_migration_in[t] * p_adult_list[t-1])
     # We need to make sure we have the correct index for t
     return N_adult_current
+
+
+### Code
+
+# We need to create lists I'm thinking to store the data in
+# Try this way instead of dataframes and see what happens - should still be able to replace at certain index
+
+N0 = 1000
+initial_ster_prop = 0.8
+p_f = 0.5
+s_i = 0.4
+lifespan = 72 # lifespan in months
+p_lifespan = 0.24 # proportion of dogs that reach lifespan
+initial_p_adult = 0
+l = 6
+r_r = 2
+m_net_in = 0
+k = 100000
+t0 = 0
+
+N_list = [N0]
+S_list = [N0 * initial_ster_prop]
+ster_prop_list = [initial_ster_prop] # Note we are currently saying equally likely for adult and puppy to get sterilised
+N_adult_list = [N0 * initial_p_adult]
+N_puppy_list = [N0 * (1 - initial_p_adult)]
+monthly_births_list = [0]
+t_list = [t0]
+p_adult_list = [initial_p_adult]
